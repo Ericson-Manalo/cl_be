964e5d22445e3a50238f01a0f30e30e4fd689472
alessandro-sartini <ale.sartini.98@gmail.com>
2025-11-13 10:50:23 +0100

    Aggiunto nuovo DTO CustomerRegistration e modifiche a CustomerDto#	new file:   Models/Dto/CustomerRegistrationDto.cs

---
+++ Models/Dto/CustomerRegistrationDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Models.Dto{
+    public class CustomerRegistrationDto
+    {
+        [Required, MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required, MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Required, EmailAddress, MaxLength(50)]
+        public string EmailAddress { get; set; }
+
+        [Required, MinLength(8)]
+        public string Password { get; set; }
+
+        [Phone, MaxLength(25)]
+        public string? Phone { get; set; }
+    }
+
+}
