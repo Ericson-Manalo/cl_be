1e95502210efe0915fbdff93938b1dbd94f7b2e6
ivanoice09 <ivankovicsibsion@gmail.com>
2025-11-22 18:31:46 +0100

    Creato una nuova cartella 'Auth' per gestire data model per l'autenticazione

---
+++ Models/Auth/LoginCredentials.cs
@@ -0,0 +1,11 @@
+﻿namespace cl_be.Models.Auth
+{
+    public class LoginCredentials
+    {
+        // Per la verificare esistenza
+        public string Email { get; set; } = null!;
+
+        // Con questo andremo a comparare con l'hash e il salt
+        public string Password { get; set; }
+    }
+}
---
+++ Models/Auth/ResetPassword.cs
@@ -0,0 +1,11 @@
+﻿namespace cl_be.Models.Auth
+{
+    public class ResetPassword
+    {
+        // Per la verificare esistenza
+        public string Email { get; set; } = null!;
+
+        // Con questo andremo a realizzare l'hash e il salt
+        public string NewPassword { get; set; }
+    }
+}
