c910a053e515f0a2044946dfb983d5691cd5d1ff
ivanoice09 <ivankovicsibsion@gmail.com>
2025-12-02 10:23:58 +0100

    Added new endpoints: Logout, Refresh .Added new private methods as support, to make Refresh Tokens work

---
+++ Models/Auth/RefreshToken.cs
@@ -0,0 +1,14 @@
+﻿namespace cl_be.Models.Auth
+{
+    public class RefreshToken
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public string Token { get; set; } = default!;
+        public DateTime Expires { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public bool IsRevoked { get; set; }
+        public int TotalRefreshes { get; set; }
+    }
+}
--- Models/ClcredsDbContext.cs
+++ Models/ClcredsDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using cl_be.Models.Auth;
 using Microsoft.EntityFrameworkCore;
 
 namespace cl_be.Models;
@@ -16,6 +17,8 @@ public partial class ClcredsDbContext : DbContext
     }
 
     public virtual DbSet<UserLogin> UserLogins { get; set; }
+
+    public virtual DbSet<RefreshToken> RefreshTokens { get; set; }
 
 //    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
